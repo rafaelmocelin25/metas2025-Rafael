1c78f1ca68740275b6090fde5d6d46a2e3e86bcb
rafaelmocelin25 <141454436+rafaelmocelin25@users.noreply.github.com>
2025-04-09 08:31:05 -0300

    adasd

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-06-30T00:12:69");
+const tempoObjetivo1 = new Date("2025-06-30T00:00:00");
 const tempoObjetivo2 = new Date("2025-11-18T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
